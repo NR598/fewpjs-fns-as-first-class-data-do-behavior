6afbd845636ae8486a44a2ae884861b2ebd010d9
Norman R <71117926+NR598@users.noreply.github.com>
2021-08-20 04:00:27 -0400

    complete

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+function greet(time) {
+// create a new variable named timestring. convert function argument into string. assign to string.
+  let string = parseInt(time)
+// if string is less then 11 return "Good Morning"
+  if (string < 12) {
+    return "Good Morning"
+  }
+// if string is greater then 16 return "Good Evening"
+  if (string > 16) {
+    return "Good Evening"
+  }
+// if string is less than 16 and greater than 11 return "Good Afternoon"
+  if (string < 17 & string > 11) {
+    return "Good Afternoon"
+  }
+}
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+
+// displayMessage() function
+// 4) updates the DOM with an appropriate message
+function displayMessage(msg) {
+  let node = document.querySelector("#greeting");
+  node.innerHTML = msg;
+  // document.body.appendChild(node);
+}
+
+
